ea7e8888b7bf24cb33f15e098ebc0a7d31c73d4d
veernitin <nitinkmr.4169@gmail.com>
2025-04-28 20:18:03 +0530

    Create oop version of cart

--- test-jasmine/tests-simple/moneyTest.js
+++ test-jasmine/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 console.log("test suite:formatCurrency");
 
@@ -6,8 +6,8 @@ console.log("convert cents into dollars");
 
 if (formatCurrency(2095) === "20.95") {
   console.log("passed");
-} else {-
-  console.log("failed");
+} else {
+  -console.log("failed");
 }
 
 console.log("works with 0");
@@ -17,8 +17,7 @@ if (formatCurrency(0) === "0.00") {
 } else {
   console.log("failed");
 }
-+
-console.log("rounds up to the nearest cent");
++console.log("rounds up to the nearest cent");
 
 if (formatCurrency(2000.5) === "20.01") {
   console.log("passed");
