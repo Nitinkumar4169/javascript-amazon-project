6a03c7a161fff3cc7e3eb17d9a62d8b5d9b2e3a2
veernitin <nitinkmr.4169@gmail.com>
2025-05-01 15:33:59 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -102,9 +102,14 @@ export function loadProductsFetch() {
         return new Product(productDetails);
       });
       console.log("load products");
+    })
+    .catch((error) => {
+      console.log("unexpected error. Please try again later.");
     });
+
   return promise;
 }
+
 /*
 loadProductsFetch().then(() => {
   console.log("next step");
@@ -123,6 +128,10 @@ export function loadProducts(fun) {
     });
     console.log("load products");
     fun();
+  });
+
+  xhr.addEventListener("error", (error) => {
+    console.log("unexpected error. Please try again later.");
   });
 
   xhr.open("GET", "https://supersimplebackend.dev/products");
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -7,13 +7,21 @@ import { loadCart } from "../data/cart.js";
 //import "../data/Backend-practice.js";
 
 async function loadPage() {
-  await loadProductsFetch();
+  try {
+    // throw "error1";
+    await loadProductsFetch();
 
-  const value = await new Promise((resolve) => {
-    loadCart(() => {
-      resolve("value3");
+    const value = await new Promise((resolve, reject) => {
+      // throw "error2";
+      loadCart(() => {
+        // reject('error3');
+        resolve("value3");
+      });
     });
-  });
+  } catch (error) {
+    console.log("unexpected error. Please try again later.");
+  }
+
   renderOrderSummary();
   renderPaymentSummary();
 }
